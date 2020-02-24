0ba29684ea36e92b6bf2537fa443dcb7f82b1013
isctorres <isctorres@gmail.com>
2020-02-24 11:28:11 -0600

    creacion del repositorio

---
+++ app/src/main/java/com/example/frutasapp/Fruta.kt
@@ -0,0 +1,10 @@
+package com.example.frutasapp
+
+class Fruta (nombre:String, Imagen:Int){
+    var nombre:String =""
+    var Imagen:Int = 0
+    init{
+        this.nombre = nombre
+        this.Imagen = Imagen
+    }
+}
